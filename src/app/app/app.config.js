062ac2a43cccf32d3277d31e828e9113f6b3ed59
kassem.ali <kassem.ali@innovapost.com>
2018-04-15 20:56:34 -0400

    embedded db mode

--- src/app/app/app.config.js
+++ src/app/app/app.config.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    "isProdMode": true
+    "isProdMode": false
 };
 //# sourceMappingURL=app.config.js.map
